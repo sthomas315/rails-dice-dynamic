8d98e5172d328d38527648eebda3797d3bc21371
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-25 14:49:58 +0000

    yes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get("/dice/:number_of_dice/:how_many_sides", {:controller=> new, :action = "user_input"}
-  get("/dice/1/20", {:controller=> "new", :action => "num_1"}
-  get("/dice/2/6", {:controller=> "new", :action => "num_2"}
-  get("/dice/5/4", {:controller=> "new", :action => "num_3"}
-  get("/dice/2/10", {:controller=> "new", :action => "num_4"}
-  get ("/"), {:controller => "new", :action => "homepage"}
+  get("/dice/:number_of_dice/:how_many_sides", {:controller=> "new", :action => "user_input"})
+  get("/dice/1/20", {:controller=> "new", :action => "num_1"})
+  get("/dice/2/6", {:controller=> "new", :action => "num_2"})
+  get("/dice/5/4", {:controller=> "new", :action => "num_3"})
+  get("/dice/2/10", {:controller=> "new", :action => "num_4"})
+  get ("/"), {:controller => "new", :action => "homepage"})
 
 end
